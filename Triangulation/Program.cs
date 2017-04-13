f1c90c7f84ec264f220b7284a0e9652b9f1a4e43
julian <0911707@hr.nl>
2017-04-13 10:27:03 +0200

    cleaned up project

--- Triangulation/Program.cs
+++ Triangulation/Program.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Triangulation
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Triangulation{
+    class Program{
+        static void Main(string[] args){
             var A = new Vector2(11, 7);
             var B = new Vector2(24, 15);
             var C = new Vector2(14, 20);
@@ -23,8 +20,6 @@ namespace Triangulation
             var x = (W * (C.Y - B.Y) - Z * (B.Y - A.Y)) / (2 * ((B.X - A.X) * (C.Y - B.Y) - (C.X - B.X) * (B.Y - A.Y)));
             var y = (W - 2 * x * (B.X - A.X)) / (2 * (B.Y - A.Y));
             var y2 = (Z - 2 * x * (C.X - B.X)) / (2 * (C.Y - B.Y));
-
-
             y = (y + y2) / 2;
 
             var P = new Vector2(x, y);
@@ -33,39 +28,18 @@ namespace Triangulation
         }
 
         public static float Pow(float i) { return i * i; }
-
-        public static Vector2 GetLocation(Dictionary<string, Circle>)
-        {
-
-        }
     }
 
-
-    public class Vector2
-    {
+    public class Vector2{
         public readonly float X;
         public readonly float Y;
-        public Vector2(float x, float y)
-        {
+        public Vector2(float x, float y){
             X = x;
             Y = y;
         }
 
-        public override string ToString()
-        {
+        public override string ToString(){
             return "X: " + Math.Round(X, 0).ToString() + "\nY: " + Math.Round(Y, 0).ToString();
-        }
-    }
-
-    public class Circle
-    {
-        public readonly Vector2 Center;
-        public readonly float Radius;
-
-        public Circle(Vector2 c, float r)
-        {
-            Center = c;
-            Radius = r;
         }
     }
 }
